009c930c1f7f85fe7d6036550c06cdcc36391995
JunhoBaek <bemodesty306@naver.com>
2022-04-05 19:50:04 +0900

    ch6

--- learn-express/app.js
+++ learn-express/app.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config();
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
@@ -26,20 +28,14 @@ app.use(
     name: "session-cookie",
   })
 );
+
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
 app.use((req, res, next) => {
-  console.log("모든 요청에 다 실행됩니다.");
-  next();
+  res.status(404).send("Not Found");
 });
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("GET / 요청에서만 실행됩니다.");
-    next();
-  },
-  (req, res) => {
-    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
-  }
-);
+
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send(err.message);
