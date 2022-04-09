f8531ff7b78e23b211607e49e09b5dbe81e6d41d
JunhoBaek <bemodesty306@naver.com>
2022-04-10 00:01:01 +0900

    mongoose-2

--- learn-mongoose/app.js
+++ learn-mongoose/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexROuter);
+app.use("/", indexRouter);
 app.use("/users", userRouter);
 app.use("/comments", commentRouter);
 
---
+++ learn-mongoose/routes/commnets.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const Comment = require("../schemas/comment");
+
+const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const comment = await Comment.create({
+      commenter: req.body.id,
+      comment: req.body.comment,
+    });
+    console.log(comment);
+    const result = await Comment.populate(comment, { path: "commenter" });
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const result = await Comment.update(
+      {
+        _id: req.params.id,
+      },
+      {
+        comment: req.body.comment,
+      }
+    );
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const result = await Comment.remove({ _id: req.params.id });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
