e6534a9e966f77802208ab99b5e64687812f8266
JunhoBaek <bemodesty306@naver.com>
2022-04-14 16:17:48 +0900

    Nodebird 마무리

--- node-bird/routes/page.js
+++ node-bird/routes/page.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const { Post, User, Hashtag } = require("../models/index");
 
 const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  res.locals.followerCount = 0;
-  res.locals.followingCount = 0;
-  res.locals.followingIdList = [];
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followingIdList = req.user
+    ? req.user.Followings.map((f) => f.id)
+    : [];
   next();
 });
 
@@ -19,12 +22,45 @@ router.get("/join", isNotLoggedIn, (req, res) => {
   res.render("join", { title: "signup - Nodebird" });
 });
 
-router.get("/", (req, res, next) => {
-  const twits = [];
-  res.render("main", {
-    title: "NodeBird",
-    twits,
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get("/hashtag", async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect("/");
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+
+    return res.render("main", {
+      title: `${query} | NodeBird`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
 module.exports = router;
