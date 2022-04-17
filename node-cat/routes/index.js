989c9f59bbf176dcab38af481d5879174d6634a6
JunhoBaek <bemodesty306@naver.com>
2022-04-17 22:31:07 +0900

    sns-api server-1

--- node-cat/routes/index.js
+++ node-cat/routes/index.js
@@ -2,28 +2,50 @@ const express = require("express");
 const axios = require("axios");
 
 const router = express.Router();
-
-router.get("/test", async (req, res, next) => {
+const URL = "http://localhost:8002/v1";
+axios.defaults.headers.origin = "http://localhost:4000";
+const request = async (req, api) => {
   try {
     if (!req.session.jwt) {
-      const tokenResult = await axios.post("http://localhost:8002/v1/token", {
+      const tokenResult = await axios.post(`${URL}/token`, {
         clientSecret: process.env.CLIENT_SECRET,
       });
-      if (tokenResult.data && tokenResult.data.code === 200) {
-        req.session.jwt = tokenResult.data.token;
-      } else {
-        return res.json(tokenResult.data);
-      }
+      req.session.jwt = tokenResult.data.token;
     }
-    const result = await axios.get("http://localhost:8002/v1/test", {
+    return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
     });
-    return res.json(result.data);
   } catch (error) {
     if (error.response.status === 419) {
-      return res.json(error.response.data);
+      delete req.session.jwt;
+      return request(req, api);
     }
-    return next(error);
+    return error.response;
+  }
+};
+
+router.get("/mypost", async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/search/:hashtag", async (req, res, next) => {
+  try {
+    const result = await request(
+      req,
+      `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
+    }
   }
 });
 
